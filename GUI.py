f3b0834fa14915e54cf7a56bce07fd68d1ccf616
YuanC <2805862297@qq.com>
2025-03-24 14:07:51 +0800

    Update GUI.py

--- GUI.py
+++ GUI.py
@@ -16,7 +16,7 @@ from qfluentwidgets import (FluentWindow, NavigationInterface, NavigationItemPos
                            StrongBodyLabel, BodyLabel, CheckBox, RadioButton, Slider, 
                            PushButton, ProgressBar, ComboBox, LineEdit, setThemeColor,
                            SmoothScrollArea, TitleLabel, SubtitleLabel, CardWidget)
-import core.core
+from core import *
 
 class ProgressBarWorker(QThread):
     """处理后台任务的工作线程"""
